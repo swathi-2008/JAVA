bbb8b347fe14765c106752b62c19538e129aa133
swathi-2008 <kkssswathi7@gmail.com>
2026-07-21 22:53:26 +0530

    Convert 01_basics from submodule to normal folder

---
+++ 01_basics/First.java
@@ -0,0 +1,12 @@
+public class First {
+
+    public static void main(String[] args) {
+
+        System.out.println("Name      : Swati K");
+        System.out.println("Department: Information Technology");
+        System.out.println("College   : Dr. Mahalingam College of Engineering and Technology");
+        System.out.println("Goal      : Become a Cloud Engineer");
+    }
+
+}
+
