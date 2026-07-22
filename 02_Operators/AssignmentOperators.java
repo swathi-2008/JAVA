5af3752bdbb67535d70103ac56c7377c336eb760
swathi-2008 <kkssswathi7@gmail.com>
2026-07-22 22:30:44 +0530

    Day 9: Added Assignment Operators example

---
+++ 02_Operators/AssignmentOperators.java
@@ -0,0 +1,14 @@
+public class AssignmentOperators {
+    public static void main(String[] args) {
+        int a = 10;
+        System.out.println("Initial value of a = " + a);
+        a += 5;   
+        System.out.println("After += 5 : " + a);
+        a -= 3;   
+        System.out.println("After -= 3 : " + a);
+        a /= 4;   
+        System.out.println("After /= 4 : " + a);
+        a %= 3;   
+        System.out.println("After %= 3 : " + a);
+    }
+}
