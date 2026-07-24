635d820d817f0c56efc44f79c6fabe049e90f6fc
swathi-2008 <kkssswathi7@gmail.com>
2026-07-24 22:16:18 +0530

    learn about comparison operators

---
+++ 02_Operators/ComparisonOperators.java
@@ -0,0 +1,14 @@
+public class ComparisonOperators {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+        System.out.println("a == b : " + (a == b));
+        System.out.println("a != b : " + (a != b));
+        System.out.println("a > b  : " + (a > b));
+        System.out.println("a < b  : " + (a < b));
+        System.out.println("a >= b : " + (a >= b));
+        System.out.println("a <= b : " + (a <= b));
+    }
+}
