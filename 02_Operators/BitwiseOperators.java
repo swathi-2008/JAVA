a86aaed5d5f81b47291d17dda583a79c468bdb24
swathi-2008 <kkssswathi7@gmail.com>
2026-07-27 22:35:00 +0530

    changes made

--- 02_Operators/BitwiseOperators.java
+++ 02_Operators/BitwiseOperators.java
@@ -1,27 +1,19 @@
 public class BitwiseOperators {
     public static void main(String[] args) {
-
-        int a = 5;   // Binary: 0101
-        int b = 3;   // Binary: 0011
-
+        int a = 5; 
+        int b = 3;   
         System.out.println("a = " + a);
         System.out.println("b = " + b);
-
         System.out.println("\nBitwise AND (&)");
         System.out.println("a & b = " + (a & b));
-
         System.out.println("\nBitwise OR (|)");
         System.out.println("a | b = " + (a | b));
-
         System.out.println("\nBitwise XOR (^)");
         System.out.println("a ^ b = " + (a ^ b));
-
         System.out.println("\nBitwise NOT (~)");
         System.out.println("~a = " + (~a));
-
         System.out.println("\nLeft Shift (<<)");
         System.out.println("a << 1 = " + (a << 1));
-
         System.out.println("\nRight Shift (>>)");
         System.out.println("a >> 1 = " + (a >> 1));
     }
