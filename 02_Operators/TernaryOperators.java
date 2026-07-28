c8fa5c0543542617cc4ac54096bf64f2cd85c9ed
swathi-2008 <kkssswathi7@gmail.com>
2026-07-28 20:55:06 +0530

    learn about ternary opereators

---
+++ 02_Operators/TernaryOperators.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class TernaryOperators {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int number = sc.nextInt();
+
+        String result = (number % 2 == 0) ? "Even Number" : "Odd Number";
+
+        System.out.println(result);
+
+        sc.close();
+    }
+}
