50949c26d436178fb4ff3f2b849d0d24c1bbdc6a
swathi-2008 <kkssswathi7@gmail.com>
2026-07-28 20:57:12 +0530

    changes made under ternary operators

--- 02_Operators/TernaryOperators.java
+++ 02_Operators/TernaryOperators.java
@@ -1,18 +1,11 @@
 import java.util.Scanner;
-
 public class TernaryOperators {
-
     public static void main(String[] args) {
-
         Scanner sc = new Scanner(System.in);
-
         System.out.print("Enter a number: ");
         int number = sc.nextInt();
-
         String result = (number % 2 == 0) ? "Even Number" : "Odd Number";
-
         System.out.println(result);
-
         sc.close();
     }
 }
