0888a989b7d504cd134426dd49c62f1804d1e35d
swathi-2008 <kkssswathi7@gmail.com>
2026-07-30 21:18:48 +0530

    learn about conditional statements if-else

---
+++ 03_ControlStatements/IfElseExample.java
@@ -0,0 +1,12 @@
+public class IfElseExample {
+    public static void main(String[] args) {
+
+        int age = 20;
+
+        if (age >= 18) {
+            System.out.println("You are eligible to vote.");
+        } else {
+            System.out.println("You are not eligible to vote.");
+        }
+    }
+}
