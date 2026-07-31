c41de4ce9ad63d3e3154c282cee476a41edc9ab3
swathi-2008 <kkssswathi7@gmail.com>
2026-07-31 21:37:29 +0530

    learn about conditional statements Elseif Ladder

---
+++ 03_ControlStatements/ElseIfLadder.java
@@ -0,0 +1,18 @@
+public class ElseIfLadder{
+    public static void main(String[] args) {
+
+        int marks = 85;
+
+        if (marks >= 90) {
+            System.out.println("Grade A");
+        } else if (marks >= 75) {
+            System.out.println("Grade B");
+        } else if (marks >= 60) {
+            System.out.println("Grade C");
+        } else if (marks >= 40) {
+            System.out.println("Grade D");
+        } else {
+            System.out.println("Grade F");
+        }
+    }
+}
