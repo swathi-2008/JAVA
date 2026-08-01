468fe8c5a8d52c7ac0f3c97073ae7303d17b1b1b
swathi-2008 <kkssswathi7@gmail.com>
2026-08-01 21:23:26 +0530

    learn about Loops

---
+++ 04_Loops/ForLoops.java
@@ -0,0 +1,8 @@
+public class ForLoops {
+    public static void main(String[] args) {
+
+        for (int number = 1; number <= 5; number++) {
+            System.out.println(number);
+        }
+    }
+}
