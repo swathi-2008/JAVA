b56567a8fb8194a9a81ec18edd053d6508a19847
swathi-2008 <kkssswathi7@gmail.com>
2026-08-12 21:12:47 +0530

    practice related in the topic reverse

---
+++ 04_Loops/Partice_problem/Reverse.java
@@ -0,0 +1,14 @@
+package Partice_problem;
+
+public class Reverse {
+    public static void main(String args[]){
+        int num=1234;
+        int rev=0;
+        while(num!=0){
+            int digit=num%10;
+            num=num/10;
+            rev=rev*10+digit;
+        }
+        System.out.println("the reversed number is:"+rev);
+    }
+}
