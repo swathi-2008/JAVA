674b2e0fcf6b635b68f61a0cfd5767404e41689c
swathi-2008 <kkssswathi7@gmail.com>
2026-08-12 21:23:06 +0530

    changes made in palindrome problem

---
+++ 04_Loops/Partice_problem/Palindrome.java
@@ -0,0 +1,22 @@
+package Partice_problem;
+import java.util.Scanner;
+public class Palindrome{
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int number = scanner.nextInt();
+        int originalNumber = number;
+        int reverse = 0;
+        while (number != 0) {
+            int digit = number % 10;
+            reverse = reverse * 10 + digit;
+            number = number / 10;
+        }
+        if (originalNumber == reverse) {
+            System.out.println("Palindrome Number");
+        } else {
+            System.out.println("Not a Palindrome Number");
+        }
+        scanner.close();
+    }
+}
