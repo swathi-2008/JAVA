e6e7261c315497592fd6a7e1d8fb9456315a72ef
swathi-2008 <kkssswathi7@gmail.com>
2026-08-12 21:02:40 +0530

    pratice related to the topic LOOPS

---
+++ 04_Loops/Partice_problem/sumEven.java
@@ -0,0 +1,13 @@
+package Partice_problem;
+
+public class sumEven {
+    public static void main(String[] args){
+        int sum=0;
+        for(int i=0;i<=10;i++){
+            if(i%2==0){
+                sum =sum+i;
+            }
+        }
+        System.out.println("the sum is:"+sum);
+    }
+}
