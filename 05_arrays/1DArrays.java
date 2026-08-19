6414f211ef4ca810933b4aad83e77d13a716c0da
swathi-2008 <kkssswathi7@gmail.com>
2026-08-19 22:45:39 +0530

    Add Java arrays basics

---
+++ 05_arrays/1DArrays.java
@@ -0,0 +1,8 @@
+public class 1DArrays {
+    public static void main(String[] args) {
+        int[] marks = {80, 75, 90, 85, 95};
+        System.out.println("Before update: " + marks[1]);
+        marks[1] = 88;
+        System.out.println("After update: " + marks[1]);
+    }
+}
