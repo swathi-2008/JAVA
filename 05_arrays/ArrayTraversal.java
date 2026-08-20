4d220214b99e5b5b94eb1d603499b70d936425d8
swathi-2008 <kkssswathi7@gmail.com>
2026-08-20 22:01:35 +0530

    slove problem about Array Traversal

---
+++ 05_arrays/ArrayTraversal.java
@@ -0,0 +1,15 @@
+public class ArrayTraversal {
+    public static void main(String[] args) {
+        int[][] matrix = {
+            {10, 20, 30},
+            {40, 50, 60},
+            {70, 80, 90}
+        };
+        for (int[] row : matrix) {
+            for (int value : row) {
+                System.out.print(value + " ");
+            }
+            System.out.println();
+        }
+    }
+}
