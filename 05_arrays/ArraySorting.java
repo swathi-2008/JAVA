837db501c160e6c8a955088148d0664c51717a8d
swathi-2008 <kkssswathi7@gmail.com>
2026-08-25 20:32:37 +0530

    learn to solve problem to sort an array

---
+++ 05_arrays/ArraySorting.java
@@ -0,0 +1,18 @@
+public class ArraySorting {
+    public static void main(String[] args) {
+        int[] numbers = {40, 10, 30, 20, 50};
+        for (int pass = 0; pass < numbers.length - 1; pass++) {
+            for (int index = 0; index < numbers.length - 1 - pass; index++) {
+                if (numbers[index] > numbers[index + 1]) {
+                    int temporary = numbers[index];
+                    numbers[index] = numbers[index + 1];
+                    numbers[index + 1] = temporary;
+                }
+            }
+        }
+        System.out.println("Sorted array:");
+        for (int number : numbers) {
+            System.out.print(number + " ");
+        }
+    }
+}
